0556ecd876fbb1860141920a9076a167af04697c
David Mears <kieukmail@gmail.com>
2019-04-09 17:00:35 +0000

    add near-empty page at /donor/add

--- projectname/receipt_generator/urls.py
+++ projectname/receipt_generator/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.receipt_generator_index, name="project_index"),
+    path("", views.receipt_generator_index, name="receipt_generator_index"),
+    path("donor/add", views.add_donor, name="add_donor")
     ]
--- projectname/receipt_generator/views.py
+++ projectname/receipt_generator/views.py
@@ -5,3 +5,7 @@ def receipt_generator_index(request):
         'welcome_message': 'Hello World!'
     }
     return render(request, 'index.html', context)
+    
+def add_donor(request):
+    context = {}
+    return render(request, 'add_donor.html', context)
